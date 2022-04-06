83a59146cc360f85476725ef37ed26a953b00411
rochafialvin <rochafi.dev@gmail.com>
2022-04-06 13:27:58 +0700

    Hallo guys

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Mas Dino </h1>");
+  res.send("<h1>Hello Guys </h1>");
 });
 
 app.listen(port, () => {
