52395ff5948814ab78daf16d92b7839d1c7fcfb5
rochafialvin <rochafi.dev@gmail.com>
2022-04-06 14:54:33 +0700

    Password is real

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Ganti ssh</h1>");
+  res.send("<h1>Hello emang ga bisa make ssh</h1>");
 });
 
 app.listen(port, () => {
